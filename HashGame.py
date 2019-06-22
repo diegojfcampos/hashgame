42bac12178e3348ed29997938d35615ee2c91930
Diego J F Campos <diegojfcampos@gmail.com>
2019-06-22 04:24:31 -0300

    The language was changed from Português for English.

--- HashGame.py
+++ HashGame.py
@@ -3,25 +3,25 @@
 
 "Globals"
 
-tabuleiro = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-posicao = 0
-jogo = True
-jogador = 1
-jogadas = 9
+board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+position = 0
+game = True
+player = 1
+
 
 """IMPORT´s"""
 
 
 """Def's"""
 
-def printTabuleiro(tabuleiro):
-    print("---TABULEIRO---")
+def DesignBoard(tabuleiro):
+    print("  GAME BOARD  ")
     tab = ""
     for i in range(0, 9):
         mark = " "
-        if tabuleiro[i] == 1:
+        if board[i] == 1:
             mark = "X"
-        elif tabuleiro[i] == -1:
+        elif board[i] == -1:
             mark = "0"
         tab += " | " + mark
 
@@ -31,99 +31,89 @@ def printTabuleiro(tabuleiro):
     print("---------------")
 
 
-def restart(reinicia):
-                global tabuleiro
-                global posicao
-                global jogo
-                global jogador
-                global jogadas
+def Restart(restart):
+    global board
+    global position
+    global game
+    global player
 
-                tabuleiro[0:9] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-                posicao = 0
-                jogo = True
-                jogador = 1
-                jogadas = 9
-                print("==============")
-                print("GAME RESTARTED")
-                print("==============")
-                printTabuleiro(tabuleiro)
+    board[0:9] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    position = 0
+    game = True
+    player = 1
+    print(f"===============\n GAME RESTARTED\n===============")
+    DesignBoard(board)
 
 
 
-def teste(teste):
-            soma1 = tabuleiro[0] + tabuleiro[1] + tabuleiro[2]
-            soma2 = tabuleiro[3] + tabuleiro[4] + tabuleiro[5]
-            soma3 = tabuleiro[6] + tabuleiro[7] + tabuleiro[8]
-            soma4 = tabuleiro[0] + tabuleiro[4] + tabuleiro[8]
-            soma5 = tabuleiro[2] + tabuleiro[4] + tabuleiro[6]
-            soma6 = tabuleiro[0] + tabuleiro[3] + tabuleiro[6]
-            soma7 = tabuleiro[1] + tabuleiro[4] + tabuleiro[7]
-            soma8 = tabuleiro[2] + tabuleiro[5] + tabuleiro[8]
+def Test(test):
+    test1 = board[0] + board[1] + board[2]
+    test2 = board[3] + board[4] + board[5]
+    test3 = board[6] + board[7] + board[8]
+    test4 = board[0] + board[4] + board[8]
+    test5 = board[2] + board[4] + board[6]
+    test6 = board[0] + board[3] + board[6]
+    test7 = board[1] + board[4] + board[7]
+    test8 = board[2] + board[5] + board[8]
 
-            if soma1 == 3 or soma2 == 3 or soma3 == 3 or soma4 == 3 or soma5 == 3 or soma6 == 3 or soma7 == 3 or soma8 == 3:
-                print("JOGADOR 1 VENCEU")
-                restart(tabuleiro)
-            elif soma1 == -3 or soma2 == -3 or soma3 == -3 or soma4 == -3 or soma5 == -3 or soma6 == -3 or soma7 == -3 or soma8 == -3:
-                print("JOGADOR 2 VENCEU")
-                restart(tabuleiro)
+    if test1 == 3 or test2 == 3 or test3 == 3 or test4 == 3 or test5 == 3 or test6 == 3 or test7 == 3 or test8 == 3:
+        print("PLAYER 1 WIN")
+        Restart(board)
+    elif test1 == -3 or test2 == -3 or test3 == -3 or test4 == -3 or test5 == -3 or test6 == -3 or test7 == -3 or test8 == -3:
+        print("PLAYER 2 WIN")
+        Restart(board)
 
 
 
-def empate(empate):
-                soma1 = tabuleiro[0] + tabuleiro[1] + tabuleiro[2]
-                soma2 = tabuleiro[3] + tabuleiro[4] + tabuleiro[5]
-                soma3 = tabuleiro[6] + tabuleiro[7] + tabuleiro[8]
-                soma4 = tabuleiro[0] + tabuleiro[4] + tabuleiro[8]
-                soma5 = tabuleiro[2] + tabuleiro[4] + tabuleiro[6]
-                soma6 = tabuleiro[0] + tabuleiro[3] + tabuleiro[6]
-                soma7 = tabuleiro[1] + tabuleiro[4] + tabuleiro[7]
-                soma8 = tabuleiro[2] + tabuleiro[5] + tabuleiro[8]
-
-                if soma1 != 0 and soma1 > -3 and soma1 < 3:
-                    if soma2 != 0 and soma2 > -3 and soma2 < 3:
-                        if soma3 != 0 and soma3 > -3 and soma3 < 3:
-                            if soma4 != 0 and soma4 > -3 and soma4 < 3:
-                                if soma5 != 0 and soma5 > -3 and soma5 < 3:
-                                    if soma6 != 0 and soma6 > -3 and soma5 < 3:
-                                        if soma7 != 0 and soma7 > -3 and soma5 < 3:
-                                            if soma8 != 0 and soma8 > -3 and soma5 < 3:
-                                                print("EMPATE")
-                                                restart(tabuleiro)
+def Drawn(drwan):
+    test1 = board[0] + board[1] + board[2]
+    test2 = board[3] + board[4] + board[5]
+    test3 = board[6] + board[7] + board[8]
+    test4 = board[0] + board[4] + board[8]
+    test5 = board[2] + board[4] + board[6]
+    test6 = board[0] + board[3] + board[6]
+    test7 = board[1] + board[4] + board[7]
+    test8 = board[2] + board[5] + board[8]
+    if test1 != 0 and test1 > -3 and test1 < 3:
+        if test2 != 0 and test2 > -3 and test2 < 3:
+            if test3 != 0 and test3 > -3 and test3 < 3:
+                if test4 != 0 and test4 > -3 and test4 < 3:
+                    if test5 != 0 and test5 > -3 and test5 < 3:
+                        if test6 != 0 and test6 > -3 and test6 < 3:
+                            if test7 != 0 and test7 > -3 and test7 < 3:
+                                if test8 != 0 and test8 > -3 and test8 < 3:
+                                    print("     DRAWN     ")
+                                    Restart(board)
 
 
 
 #JOGO
 
-while jogo == True:
-        teste(tabuleiro)
-        if jogador == 1:
-            print("JOGADOR 1")
+while game == True:
+        Test(board)
+        if player == 1:
+            print("  PLAYER 1")
         else:
-            print("JOGADOR 2")
-        posicao = int(input("Digite a posição de 0 a 8: "))
-        if posicao < 0 or posicao > 8:
-            print("Posição inválida, digite de 1 a 9.")
-        elif tabuleiro[posicao] == 1 or tabuleiro[posicao] == -1:
-            print("Posicão Indisponível, tente outra opção entre 0 a 8.")
+            print("  PLAYER 2")
+        position = int(input("Choose Position 0 to 8: "))
+        if position < 0 or position > 8:
+            print("Invalid Position, type: 1 to 8.")
+        elif board[position] == 1 or board[position] == -1:
+            print("Unavailable Position, try another position: 0 to 8.")
         else:
-            if jogador == 1 and tabuleiro[posicao] == 0:
-                tabuleiro[posicao] = 1
-                printTabuleiro(tabuleiro)
-                jogador = -1
-                teste(tabuleiro)
-                empate(empate)
+            if player == 1 and board[position] == 0:
+                board[position] = 1
+                DesignBoard(board)
+                player = -1
+                Test(board)
+                Drawn(board)
 
-            if jogador == -1 and tabuleiro[posicao] == 0:
-                tabuleiro[posicao] = -1
-                printTabuleiro(tabuleiro)
-                jogador = 1
-                teste(tabuleiro)
-                empate(empate)
-
-
-
-
-
+            if player == -1 and board[position] == 0:
+                board[position] = -1
+                DesignBoard(board)
+                player = 1
+                Test(board)
+                Drawn(board)
 
 
 
